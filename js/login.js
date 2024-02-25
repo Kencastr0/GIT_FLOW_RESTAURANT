4977ef1fd30b0e867886673c8b6308f8dc4311b5
Rignu1503 <rigoberto.miranda1503@gmail.com>
2024-02-25 15:05:09 -0500

    errores resuelto

--- js/login.js
+++ js/login.js
@@ -1,11 +1,24 @@
+import { credenciales } from "../database/credenciales.js";
+
 const form = document.getElementById("form-login");
-const email = document.getElementById("email").value;
-const password = document.getElementById("password").value;
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
 
-
-    localStorage.setItem("user", email.value);
-    window.location.href = "index.html";
+    validacionLogin()
 });
+
+function validacionLogin() {
+
+    if (credenciales.email === email.value && credenciales.password === password.value) {
+        localStorage.setItem("user", email.value);
+        window.location.href = "index.html";
+        
+    }else{
+        alert("correo o contraseña incorrectos")
+        return;
+    }
+    
+}
